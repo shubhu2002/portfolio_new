8099bf13b2c373280e2d9de3e1a29c8aa8af7d37
shubhu2002 <shubhanshusaxena999@gmail.com>
2025-03-12 01:33:33 +0530

    remove apiInstance

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,3 @@
-import axios from "axios";
-
-const apiInstance = axios.create({
-  baseURL: "https://portfolionew-backend-production.up.railway.app/api/v1",
-});
-
 const open_link = (url: string) => window.open(url, "_blank");
 
-export { apiInstance,open_link };
+export { open_link };
